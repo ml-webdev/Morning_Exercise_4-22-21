261c8064f04f66c32f6e0de48d7623dbc0690c4e
ml-webdev <codepenml@gmail.com>
2021-04-22 21:56:52 -0400

    Initialized files
    added javascript, html, css

---
+++ app.js
@@ -0,0 +1,12 @@
+$('#question-1').click(function(){
+    $('#question-1').append("4 ")
+})
+$('#question-2').click(function(){
+    $('#question-2').append("6 ")
+})
+$('#question-3').click(function(){
+    $('#question-3').append("8 ")
+})
+$('#question-4').click(function(){
+    $('#question-4').append("42 ")
+})
